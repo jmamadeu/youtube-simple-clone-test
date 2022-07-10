16a83fea1561ab02dc6814a9a583c62eebd7db3c
jmamadeu <jmamadeu2000@gmail.com>
2022-07-10 23:23:02 +0100

    chore: add layout template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { QueryClientProvider } from "react-query";
+import { AppLayout } from "../components/app-layout";
 import { queryClient } from "../services/query-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+      <ChakraProvider>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </ChakraProvider>
     </QueryClientProvider>
   );
 }
